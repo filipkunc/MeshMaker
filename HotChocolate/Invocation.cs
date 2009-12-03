76c0bf3aa1645ba882e25c00ee393beee7bf0f3b
Filip Kunc <kunc.filip@gmail.com>
2009-12-03 22:54:22 +0100

    Working on UndoManager for Windows. C# doesn't have something similar to superior NSUndoManager so I'm creating my own version from scratch for C#.

---
+++ HotChocolate/Invocation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HotChocolate
+{
+    public class Invocation
+    {
+        object argument;
+        Delegate action;
+
+        public Invocation(object argument, Delegate action)
+        {
+            this.argument = argument;
+            this.action = action;
+        }
+
+        public void Perform()
+        {
+            action.DynamicInvoke(argument);
+        }
+
+        public static Invocation Create<T>(T argument,
+            Action<T> action)
+        {
+            return new Invocation(argument, action);
+        }
+
+        public static Invocation Create<T1, T2>(T1 arg1, T2 arg2,
+            Action<T1, T2> action)
+        {
+            return Create(new { A1 = arg1, A2 = arg2 },
+                args => action(args.A1, args.A2));
+        }
+
+        public static Invocation Create<T1, T2, T3>(T1 arg1, T2 arg2, T3 arg3,
+            Action<T1, T2, T3> action)
+        {
+            return Create(new { A1 = arg1, A2 = arg2, A3 = arg3 },
+                args => action(args.A1, args.A2, args.A3));
+        }
+
+        public static Invocation Create<T1, T2, T3, T4>(T1 arg1, T2 arg2, T3 arg3, T4 arg4,
+            Action<T1, T2, T3, T4> action)
+        {
+            return Create(new { A1 = arg1, A2 = arg2, A3 = arg3, A4 = arg4 },
+                args => action(args.A1, args.A2, args.A3, args.A4));
+        }
+    }
+}
---
+++ HotChocolate/UndoManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HotChocolate
+{
+    public class UndoManager
+    {
+        Stack<Invocation> undoStack;
+        Stack<Invocation> redoStack;
+        bool undoing;
+
+        public UndoManager()
+        {
+            undoStack = new Stack<Invocation>();
+            redoStack = new Stack<Invocation>();
+            undoing = false;
+        }
+
+        public bool CanUndo
+        {
+            get { return undoStack.Count > 0; }
+        }
+
+        public bool CanRedo
+        {
+            get { return redoStack.Count > 0; }
+        }
+
+        public void PrepareUndo(Invocation invocation)
+        {
+            if (undoing)
+                redoStack.Push(invocation);
+            else
+                undoStack.Push(invocation);
+        }
+
+        public void Undo()
+        {
+            if (CanUndo)
+            {
+                Invocation invocation = undoStack.Pop();
+                undoing = true;
+                invocation.Perform();
+                undoing = false;
+            }
+        }
+
+        public void Redo()
+        {
+            if (CanRedo)
+            {
+                Invocation invocation = redoStack.Pop();
+                invocation.Perform();
+            }
+        }
+    }
+}
