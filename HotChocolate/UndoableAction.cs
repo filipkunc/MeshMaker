871d7527c68d1374e8e657b76bebcac4a4186242
Filip Kunc <kunc.filip@gmail.com>
2009-12-25 00:56:37 +0100

    Some work on needs save for UndoManager.

---
+++ HotChocolate/UndoableAction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HotChocolate
+{
+    public class UndoableAction
+    {
+        string name;
+        Invocation invocation;
+
+        public string Name { get { return name; } }
+
+        public void Perform()
+        {
+            invocation.Perform();
+        }
+
+        public UndoableAction(string name, Invocation invocation)
+        {
+            this.name = name;
+            this.invocation = invocation;
+        }
+    }
+}
