5712312d6eeb0c47cc7b5c6666be4f95ab5f798a
Filip Kunc <kunc.filip@gmail.com>
2010-01-03 00:02:49 +0100

    Fixed bugs in textboxes in Windows version.

---
+++ HotChocolate/FloatTextBox.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HotChocolate
+{
+    public class FloatTextBox : ToolStripTextBox
+    {
+        public float Number
+        {
+            get
+            {
+                float result = 0.0f;
+                if (float.TryParse(Text, out result))
+                    return result;
+                return 0.0f;
+            }
+            set
+            {
+                Text = value.ToString();
+            }
+        }
+
+        public event EventHandler NumberChanged;
+
+        protected virtual void OnNumberChanged(EventArgs e)
+        {
+            if (NumberChanged != null)
+                NumberChanged(this, e);
+        }
+
+        protected override void OnLeave(EventArgs e)
+        {
+            base.OnLeave(e);
+            OnNumberChanged(EventArgs.Empty);
+        }
+
+        protected override void OnKeyDown(KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Return)
+                OnNumberChanged(EventArgs.Empty);
+            base.OnKeyDown(e);
+        }
+    }
+}
