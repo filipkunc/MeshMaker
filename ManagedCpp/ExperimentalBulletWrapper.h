e401874807dd2a4854c223c63fd1bf30ea8b163b
Filip Kunc <kunc.filip@gmail.com>
2010-02-08 23:38:39 +0100

    Windows version:
    - Added same drawing as on Mac version
    - Texturing is for now only on first view
    
    *Texture memory is not shared.*
    
    I am considering wglShareLists or HGLRC same
    for each view, dunno which is better.

--- ManagedCpp/ExperimentalBulletWrapper.h
+++ ManagedCpp/ExperimentalBulletWrapper.h
@@ -12,6 +12,8 @@
 #include <windows.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
+#include <vector>
+using namespace std;
 #include "../PureCpp/Enums.h"
 #include "OpenGLManipulatingModel.h"
 
@@ -24,57 +26,9 @@
 #include "BulletCollision/BroadphaseCollision/btDbvtBroadphase.h"
 #include "BulletDynamics/ConstraintSolver/btSequentialImpulseConstraintSolver.h"
 #include "LinearMath/btIDebugDraw.h"
+#include "LinearMath/btDefaultMotionState.h"
+#include "Demos/OpenGL/GL_ShapeDrawer.h"
 using namespace bParse;
-
-class ExperimentalDebugDrawImplementation : public btIDebugDraw
-{
-private:
-	int debugMode;
-public:
-	ExperimentalDebugDrawImplementation()
-	{
-		debugMode = DBG_MAX_DEBUG_DRAW_MODE;
-	}
-	
-	virtual void drawLine(const btVector3 &from, const btVector3 &to, const btVector3 &color)
-	{
-		glColor3f(color.x(), color.y(), color.z());
-		
-		glBegin(GL_LINES);
-		glVertex3f(from.x(), from.y(), from.z());
-		glVertex3f(to.x(), to.y(), to.z());
-		glEnd();
-	}
-	
-	virtual void drawContactPoint(const btVector3 &PointOnB,
-								  const btVector3 &normalOnB,
-								  btScalar distance,
-								  int lifeTime,
-								  const btVector3 &color)
-	{
-		// ignored
-	}
-	
-	virtual void reportErrorWarning(const char *warningString) 
-	{ 
-		//NSLog(@"%s", warningString); 
-	}
-	
-	virtual void draw3dText(const btVector3 &location, const char *textString)
-	{ 
-		// ignored
-	}  
-	
-	virtual void setDebugMode(int debugMode) 
-	{ 
-		this->debugMode = debugMode; 
-	}
-	
-	virtual int	getDebugMode() const 
-	{ 
-		return this->debugMode; 
-	}
-};
 
 class BulletWrapperHelper
 {
@@ -85,7 +39,8 @@ public:
 	btSequentialImpulseConstraintSolver *solver;
 	btDynamicsWorld *dynamicsWorld;
 	btBulletWorldImporter *worldImporter;
-	ExperimentalDebugDrawImplementation *debugDrawer;
+	GL_ShapeDrawer *shapeDrawer;
+	vector<CocoaBool> *selection;
 
 	BulletWrapperHelper()
 	{
@@ -96,17 +51,85 @@ public:
 		dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, broadphase, solver, collisionConfiguration);
 		dynamicsWorld->setGravity(btVector3(0, -10, 0));	
 		worldImporter = new btBulletWorldImporter(dynamicsWorld);
-		debugDrawer = new ExperimentalDebugDrawImplementation();
+		shapeDrawer = new GL_ShapeDrawer();
+		selection = new vector<CocoaBool>();
 	}
 
 	bool LoadFile(const char *fileName)
 	{
 		if (worldImporter->loadFile(fileName))
 		{
-			dynamicsWorld->setDebugDrawer(debugDrawer);
+			for (int i = 0; i < dynamicsWorld->getNumCollisionObjects(); i++)
+			{
+				selection->push_back(NO);
+			}
 			return true;
 		}
 		return false;
+	}
+
+	void Draw(uint index, bool selected)
+	{
+		btScalar m[16];
+		btMatrix3x3	rot;
+		rot.setIdentity();
+		
+		btCollisionObject *colObj = dynamicsWorld->getCollisionObjectArray()[index];
+		btRigidBody *body = btRigidBody::upcast(colObj);
+		
+		if (body && body->getMotionState())
+		{
+			btDefaultMotionState *myMotionState = (btDefaultMotionState *)body->getMotionState();
+			myMotionState->m_graphicsWorldTrans.getOpenGLMatrix(m);
+			rot = myMotionState->m_graphicsWorldTrans.getBasis();
+		}
+		else
+		{
+			colObj->getWorldTransform().getOpenGLMatrix(m);
+			rot = colObj->getWorldTransform().getBasis();
+		}
+		btVector3 wireColor(1.0f, 1.0f, 0.5f); //wants deactivation
+		if (index & 1) 
+			wireColor = btVector3(0.0f, 0.0f, 1.0f);
+		///color differently for active, sleeping, wantsdeactivation states
+		if (colObj->getActivationState() == 1) //active
+		{
+			if (index & 1)
+				wireColor += btVector3(1.0f, 0.0f, 0.0f);
+			else
+				wireColor += btVector3(0.5f, 0.0f, 0.0f);
+		}
+		if (colObj->getActivationState() == 2) //ISLAND_SLEEPING
+		{
+			if (index & 1)
+				wireColor += btVector3(0.0f, 1.0f, 0.0f);
+			else
+				wireColor += btVector3(0.0f, 0.5f, 0.0f);
+		}
+		
+		btVector3 aabbMin,aabbMax;
+		dynamicsWorld->getBroadphase()->getBroadphaseAabb(aabbMin, aabbMax);
+		
+		aabbMin -= btVector3(BT_LARGE_FLOAT, BT_LARGE_FLOAT, BT_LARGE_FLOAT);
+		aabbMax += btVector3(BT_LARGE_FLOAT, BT_LARGE_FLOAT, BT_LARGE_FLOAT);
+		
+		shapeDrawer->enableTexture(true);
+		
+		if (selected)
+		{
+			glEnable(GL_POLYGON_OFFSET_FILL);
+			glPolygonOffset(1.0f, 1.0f);
+			shapeDrawer->drawOpenGL(m, colObj->getCollisionShape(), wireColor, 0, aabbMin, aabbMax);
+			glDisable(GL_POLYGON_OFFSET_FILL);
+			wireColor = btVector3(0.5f, 0.5f, 0.5f);
+			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+			shapeDrawer->drawOpenGL(m, colObj->getCollisionShape(), wireColor, 0, aabbMin, aabbMax);
+			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		}
+		else
+		{
+			shapeDrawer->drawOpenGL(m, colObj->getCollisionShape(), wireColor, 0, aabbMin, aabbMax);
+		}
 	}
 
 	~BulletWrapperHelper()
@@ -117,7 +140,8 @@ public:
 		delete solver;
 		delete dynamicsWorld;
 		delete worldImporter;
-		delete debugDrawer;
+		delete shapeDrawer;
+		delete selection;
 	}
 };
 
@@ -131,7 +155,6 @@ namespace ManagedCpp
 		BulletWrapperHelper *wrapper;
 	public:
 		ExperimentalBulletWrapper(String ^fileName);
-		void DebugDraw();
 		void StepSimulation(btScalar timeStep);
 
 		virtual property uint Count { uint get(); }
