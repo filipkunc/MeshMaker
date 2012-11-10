b93582790999f4296a338d41c5cbc831d2fafda4
Filip Kunc <kunc.filip@gmail.com>
2012-11-10 20:45:28 +0100

    Improved quadify.js

--- Scripts/quadify.js
+++ Scripts/quadify.js
@@ -1,4 +1,5 @@
-// quadify item by removing longest shared edge in two triangles 
+// quadify item by removing longest or longest opposite
+// shared edge in two triangles 
 
 function edgeSqLength(v0, v1)
 {
@@ -10,12 +11,18 @@ function edgeSqLength(v0, v1)
 	return sqLength;
 }
 
-function isLongestEdgeInTriangles(edge, t0, t1)
+function triangleHasEdgeSelected(triangle)
 {
-	var ev0 = edge.vertex(0);
-	var ev1 = edge.vertex(1);
-	var ov0 = t0.vertexNotInEdge(edge);
-	var ov1 = t1.vertexNotInEdge(edge);
+	for (var i = 0; i < 3; i++)
+	{
+		if (triangle.edge(i).selected())
+			return true;
+	}
+	return false;
+}
+
+function isLongestEdge(ev0, ev1, ov0, ov1)
+{
 	var currentEdgeSqLength = edgeSqLength(ev0, ev1);
 	
 	if (currentEdgeSqLength < edgeSqLength(ev0, ov0))
@@ -29,6 +36,26 @@ function isLongestEdgeInTriangles(edge, t0, t1)
 		return false;
 		
 	return true;
+}
+
+function isLongestEdgeInTriangles(edge, t0, t1)
+{
+	var ev0 = edge.vertex(0);
+	var ev1 = edge.vertex(1);
+	var ov0 = t0.vertexNotInEdge(edge);
+	var ov1 = t1.vertexNotInEdge(edge);
+
+	return isLongestEdge(ev0, ev1, ov0, ov1);
+}
+
+function isLongestOppositeEdgeInTriangles(edge, t0, t1)
+{
+	var ev0 = edge.vertex(0);
+	var ev1 = edge.vertex(1);
+	var ov0 = t0.vertexNotInEdge(edge);
+	var ov1 = t1.vertexNotInEdge(edge);
+	
+	return isLongestEdge(ov0, ov1, ev0, ev1);
 }
 
 function quadify(item)
@@ -47,6 +74,29 @@ function quadify(item)
         if (t0 && t1 && !t0.isQuad() && !t1.isQuad())
         {
         	if (isLongestEdgeInTriangles(edge, t0, t1))
+        		edge.setSelected(true);
+        }       
+    }
+    
+    edge.moveStart();
+    
+    for (edge; !edge.finished(); edge.moveNext())
+    {
+    	if (edge.selected())
+    		continue;
+    	
+    	var t0 = edge.triangle(0);
+    	var t1 = edge.triangle(1);
+    	
+        if (t0 && t1 && !t0.isQuad() && !t1.isQuad())
+        {
+        	if (triangleHasEdgeSelected(t0))
+        		continue;
+        		
+        	if (triangleHasEdgeSelected(t1))
+        	 	continue;
+        	
+        	if (isLongestOppositeEdgeInTriangles(edge, t0, t1))
         		edge.setSelected(true);
         }       
     }
