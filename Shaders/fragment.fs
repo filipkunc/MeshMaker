3914e78db1784e851a145a487b2e507f37ea0d83
Filip Kunc <kunc.filip@gmail.com>
2012-11-21 23:18:34 +0100

    Specular and front/back facing vertex and fragment shader.

--- Shaders/fragment.fs
+++ Shaders/fragment.fs
@@ -1,11 +1,38 @@
 // fragment.fs
 
 varying vec3 normal;
+varying vec3 eyeCoords;
 
 void main()
 {
-    vec3 n = normalize(normal);
-    float NdotL = max(dot(n, vec3(0.0, 0.0, -1.0)), 0.0);
-    gl_FragColor = gl_Color * NdotL + vec4(0.3, 0.3, 0.3, 1.0);
-    gl_FragColor.a = 0.5;
+    vec3 l, n, material;
+    
+    if (!gl_FrontFacing)
+    {
+        l = vec3(0.0, 0.0, 1.0);
+        n = normalize(-normal);
+        material = vec3(gl_Color);
+    }
+    else
+    {
+        l = vec3(0.0, 0.0, -1.0);
+        n = normalize(normal);
+        material = vec3(0.5, 0.5, 0.5);
+    }
+    
+    vec3 s = normalize(l - eyeCoords);
+    vec3 v = normalize(-eyeCoords);
+    vec3 r = reflect(-s, n);
+    
+    float sDotN = max(dot(s, n), 0.0);
+    
+    vec3 ambient = vec3(0.3, 0.3, 0.3);
+    vec3 diffuse = material * sDotN;
+    vec3 specular = vec3(0.0, 0.0, 0.0);
+    
+    if (sDotN > 0.0)
+        specular = material * pow(max(dot(r, v), 0.0), 20.0);
+    
+    vec3 finalColor = ambient + diffuse + specular;
+    gl_FragColor = vec4(finalColor, 0.5);
 }
--- Shaders/vertex.vs
+++ Shaders/vertex.vs
@@ -1,10 +1,13 @@
 // vertex.vs
 
 varying vec3 normal;
+varying vec3 eyeCoords;
 
 void main()
 {	
 	normal = normalize(gl_NormalMatrix * gl_Normal);
+    eyeCoords = vec3(gl_ModelViewMatrix * gl_Vertex);
+    
 	gl_TexCoord[0] = gl_MultiTexCoord0;
     gl_FrontColor = gl_Color;
     gl_Position = ftransform();
